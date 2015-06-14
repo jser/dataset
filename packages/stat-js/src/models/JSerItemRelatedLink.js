996413f3be7f1e91ef7bd415db981280c4189ae8
azu <azuciao@gmail.com>
2015-06-14 19:56:19 +0900

    docs(models): add JSDoc type to property

--- packages/stat-js/src/models/JSerItemRelatedLink.js
+++ packages/stat-js/src/models/JSerItemRelatedLink.js
@@ -2,7 +2,9 @@
 "use strict";
 export default class JSerItemRelatedLink {
     constructor(link) {
+        /** @type string */
         this.title = link["title"];
+        /** @type string */
         this.url = link["url"];
     }
 }
--- packages/stat-js/src/models/JSerPost.js
+++ packages/stat-js/src/models/JSerPost.js
@@ -2,9 +2,13 @@
 "use strict";
 export default class JSerPost {
     constructor(post) {
+        /** @type string */
         this.title = post["title"];
+        /** @type string */
         this.url = post["url"];
+        /** @type string */
         this.content = post["content"];
+        /** @type string */
         this.category = post["category"];
         this.date = new Date(post["date"]);
         /** @type string[] */
