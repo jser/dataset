eaa4ef2989eacd34be0a8c668449b9cdf99c9f1e
azu <azuciao@gmail.com>
2015-08-15 23:06:22 +0900

    feat(example): add example

--- packages/stat-js/example/example.js
+++ packages/stat-js/example/example.js
@@ -4,3 +4,5 @@ var assert = require("assert");
 var JSerStat = require("jser-stat").JSerStat;
 var stat = new JSerStat();
 assert(stat.getJSerWeeks().length > 0);
+var firstWeek = stat.findJSerWeek(1);
+assert(firstWeek.weekNumber === 1);
