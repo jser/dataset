5529c020744ebd344bf751c484e4f676b0571401
azu <azuciao@gmail.com>
2015-11-09 23:38:41 +0900

    test(state): add test for findRelatedItem

--- packages/stat-js/test/JSerStat-test.js
+++ packages/stat-js/test/JSerStat-test.js
@@ -151,4 +151,49 @@ describe("jser-stat", function () {
             assert(expectedItem.isEqualItem(item));
         });
     });
+    describe("#findRelatedItems", function () {
+        it("should return JSerItem[] related item", function () {
+            var posts = [
+                {
+                    "title": "2015-06-10のJS: ブラウザとES6の状況、Web Audio APIチュートリアル",
+                    "url": "http://jser.info/2015/06/10/es6-status-webaudio/",
+                    "date": "2015-06-10T12:45:00+09:00",
+                    "content": "JSer.info #231 - Safari 9.0の変更点が公開されています。JavaSc...",
+                    "category": "JSer",
+                    "tags": ["WebAudio", "ES6", "Safari", "Chrome", "MSEdge"]
+                }
+            ];
+            var items = [
+                {
+                    "title": "Changelog · winjs/winjs Wiki",
+                    "url": "https://github.com/winjs/winjs/wiki/Changelog#v40",
+                    "content": "WinJS 4.0リリース",
+                    "tags": ["JavaScript", "library", "ReleaseNote"],
+                    "date": "2015-06-10T01:28:52.936Z",
+                    "relatedLinks": []
+                },
+                {
+                    "title": "winjs",
+                    "url": "https://github.com/winjs/winjs",
+                    "content": "WinJS のリポジトリ",
+                    "tags": ["JavaScript", "library"],
+                    "date": "2015-06-11T01:28:52.936Z",
+                    "relatedLinks": []
+                },
+                {
+                    "title": "scottcorgan/immu",
+                    "url": "https://github.com/scottcorgan/immu",
+                    "content": "Immutable Objectを扱うライブラリ。\nObject.definePropertyやObject.freezeを使ってArrayやObjectの変更を防止したオブジェクトを作成する",
+                    "tags": ["JavaScript", "library"],
+                    "date": "2015-08-15T16:50:28.637Z",
+                    "relatedLinks": []
+                }
+            ];
+            var stat = new JSerStat(items, posts);
+            var item = new Item(items[0]);
+            var expectedRelatedItem = new Item(items[1]);
+            var relatedItems = stat.findRelatedItems(item);
+            assert(relatedItems[0].isEqualItem(expectedRelatedItem));
+        });
+    });
 });
