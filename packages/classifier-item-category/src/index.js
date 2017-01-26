bd8d0d669e2794bfaf32a598887daecc00b18112
azu <azuciao@gmail.com>
2017-01-26 19:31:20 +0900

    lazy load

--- packages/classifier-item-category/src/index.js
+++ packages/classifier-item-category/src/index.js
@@ -2,8 +2,14 @@
 "use strict";
 const Category = require("jser-item-category-parser").Category;
 const CategoryKey = require("jser-item-category-parser").CategoryKey;
+const ItemCategories = require("../data/item-category.json");
 const JSerClassifier = require("./classifier-item-category");
-module.exports.classifier = require("./classifier");
+module.exports = {
+    get classifier() {
+        return require("./classifier");
+    }
+};
+module.exports.ItemCategorhjhies = ItemCategories;
 module.exports.Category = Category;
 module.exports.CategoryKey = CategoryKey;
 module.exports.JSerClassifier = JSerClassifier;
