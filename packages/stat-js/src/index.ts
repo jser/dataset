15905104c98622d7213940f85636a34d652d36dd
azu <azuciao@gmail.com>
2017-10-23 11:18:28 +0900

    fix(index): export as class

--- packages/stat-js/src/index.ts
+++ packages/stat-js/src/index.ts
@@ -4,9 +4,12 @@ const _Posts = require("../data/posts.json");
 const _Items = require("../data/items.json");
 import { JSerStat as InternalJSerStat } from "./JSerStat";
 
-const JSerStat = function _JSerStat(items = _Items, posts = _Posts) {
-    return new InternalJSerStat(items, posts);
-};
+export class JSerStat {
+    constructor(items = _Items, posts = _Posts) {
+        return new InternalJSerStat(items, posts);
+    }
+}
+
 import * as compute from "./compute/compute-tags";
 
-export { JSerStat, compute };
+export { compute };
